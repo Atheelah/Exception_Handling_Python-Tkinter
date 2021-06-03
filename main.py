7958fcff5169a79f059dca88a5ab1c6994a43e9f
Atheelah <atheelahvanderschyff17@gmail.com>
2021-06-03 22:03:16 +0200

    update

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
+# IMPORTING TKINTER AND MESSAGEBOX
 from tkinter import *
 from tkinter import messagebox
 root = Tk()
+
+# SETTING THE TITLE FOR THE WIDGET
 root.title("Authentication")
+
+# SETTING THE SIZE
 root.geometry("350x250")
+
+# THE USER CANT MAXIMIZE THE WINDOW
 root.resizable(height=False, width=False)
+
+# SETTING THE BACKGROUND COLOR
 root.config(bg='grey')
+
+# THE HEADING OF THE WINDOW
 header = Label(root, text="PLEASE ENTER LOGIN DETAILS", bg='salmon')
 header.place(x=80, y=0)
+
+# THE USERNAME LABEL, ENTRY AND PLACEMENT
 username = Label(root, text="Username", bg='grey')
 username.place(x=120, y=40)
 userEnt = Entry(root)
 userEnt.place(x=80, y=60)
+
+# THE PASSWORD LABEL, ENTRY AND PLACEMENT
 password = Label(root, text="Password", bg='grey')
 password.place(x=122, y=110)
 passEnt = Entry(root)
@@ -26,6 +41,7 @@ user_pass = {"Vuyani": "vuya@2021", "Atheelah": "maroon17", "Ikraam": "carsthemo
 def user_search():
     if userEnt.get() in user_pass:
         if passEnt.get() == user_pass[userEnt.get()]:
+            # IF THE DETAILS ARE CORRECT THE CURRENT WINDOW WILL DESTROY AND NEXT WILL OPEN
             root.destroy()
             import window_2
         else:
@@ -38,5 +54,5 @@ authBtn = Button(root, text='Login', borderwidth="3", bg="salmon", command=user_
 authBtn.place(x=130, y=160)
 
 
-
+# ALLOWING THE WINDOW TO RUN
 root.mainloop()
--- window_2.py
+++ window_2.py
@@ -1,16 +1,31 @@
+# IMPORTING TKINTER AND MESSAGEBOX
 from tkinter import *
 from tkinter import messagebox
 root = Tk()
+
+# SETTING THE TITLE FOR THE WIDGET
 root.title("Balance Check")
+
+# SETTING THE SIZE
 root.geometry("350x250")
+
+# THE USER CANT MAXIMIZE THE WINDOW
 root.resizable(height=False, width=False)
+
+# SETTING THE BACKGROUND COLOR
 root.config(bg='grey')
+
+# CREATING A LABEL AND PLACING IT
 amount = Label(root, text="Please Enter Amount In Your Account", bg='grey')
 amount.place(x=40, y=20)
+
+# CREATING AN ENTRY AND PLACING IT
 amountEnt = Entry(root)
 amountEnt.place(x=80, y=60)
 
-
+# DEFINING A FUNCTION
+# IF THE BANK BALANCE IS <3000 AN ERROR MESSAGE WILL SHOW (YOU ARE BROKE)
+# IF THE BANK BALANCE IS >=3000 A CONGRATULATIONS MESSAGE WILL SHOW
 def calc():
     try:
         if int(amountEnt.get()) < 0:
@@ -23,10 +38,10 @@ def calc():
     except ValueError:
         messagebox.showerror(message="Invalid Amount")
 
-
+# CREATING A BUTTON. THE FUNCTION IS ASSIGNED TO THE BUTTON.
 checkBtn = Button(root, text="Check Qualification", borderwidth="3", bg="salmon", command=calc)
 checkBtn.place(x=80, y=100)
 
 
-
+# ALLOWING THE WINDOW TO RUN
 root.mainloop()
